4f2914300c12ed3c58299c3cd9a2196ffe7d3cb5
Howie <howie12207@gmail.com>
2021-09-12 14:44:16 +0800

    feat(banner): add banner

---
+++ src/components/common/banner/Banner.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { defineProps } from "vue";
+
+import { Swiper, SwiperSlide } from "swiper/vue";
+import SwiperCore, { Autoplay, Navigation, Pagination } from "swiper";
+import "swiper/css/bundle";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+SwiperCore.use([Autoplay, Navigation, Pagination]);
+
+defineProps({
+  bannerList: {
+    type: Array,
+    default: () => [],
+  },
+  autoplay: {
+    type: Boolean,
+    default: true,
+  },
+  autoplayTime: {
+    type: Number,
+    default: 3000,
+  },
+  loop: {
+    type: Boolean,
+    default: true,
+  },
+  navigation: {
+    type: Boolean,
+    default: true,
+  },
+  pagination: {
+    type: Boolean,
+    default: true,
+  },
+  clickable: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const imgSrc = (src) => {
+  const path = `./img/${src}`;
+  const modules = import.meta.globEager("./img/*.png");
+  return modules[path].default;
+};
+</script>
+
+<template>
+  <swiper
+    class="mySwiper"
+    :loop="loop"
+    :navigation="navigation"
+    :pagination="
+      pagination && {
+        clickable,
+      }
+    "
+    :autoplay="
+      autoplay && {
+        delay: autoplayTime,
+        disableOnInteraction: false,
+      }
+    "
+  >
+    <swiper-slide v-for="banner of bannerList" :key="banner.label">
+      <img :src="banner.src" :alt="banner.label" class="w-full"
+    /></swiper-slide>
+  </swiper>
+</template>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,9 +1,23 @@
 <script setup>
 import Header from "../../components/layout/header/Header.vue";
 import Navbar from "../../components/layout/navbar/Navbar.vue";
+import Banner from "../../components/common/banner/Banner.vue";
+
+const imgSrc = (src) => {
+  const path = `./img/${src}`;
+  const modules = import.meta.globEager("./img/*.png");
+  return modules[path].default;
+};
+
+const bannerList = [
+  { label: "banner01", src: imgSrc("banner_01.png") },
+  { label: "banner02", src: imgSrc("banner_02.png") },
+  { label: "banner03", src: imgSrc("banner_03.png") },
+];
 </script>
 
 <template>
   <Header />
   <Navbar />
+  <Banner :bannerList="bannerList" />
 </template>
