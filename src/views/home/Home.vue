b267d2ffc671f2d2e2afc9ebd5c2b2c4e00c8694
Howie <howie12207@gmail.com>
2021-09-16 22:51:48 +0800

    chore: revise data location

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,15 @@
 <script setup>
 import Header from "@/components/layout/header/Header.vue";
 import Navbar from "@/components/layout/navbar/Navbar.vue";
-import Banner from "@/components/common/banner/Banner.vue";
-import TotalFund from "@/components/pages/home/TotalFund.vue";
-import SearchCard from "@/components/pages/home/SearchCard.vue";
-import MenuCard from "@/components/pages/home/MenuCard.vue";
-import RecentView from "@/components/pages/home/RecentView.vue";
-import Marquee from "@/components/common/marquee/Marquee.vue";
-import FooterNav from "@/components/pages/home/FooterNav.vue";
-import FooterContact from "@/components/pages/home/FooterContact.vue";
-import FooterStatement from "@/components/pages/home/FooterStatement.vue";
+import Banner from "@/components/banner/Banner.vue";
+import TotalFund from "./TotalFund.vue";
+import SearchCard from "./SearchCard.vue";
+import MenuCard from "./MenuCard.vue";
+import RecentView from "./RecentView.vue";
+import Marquee from "@/components/marquee/Marquee.vue";
+import FooterNav from "./FooterNav.vue";
+import FooterContact from "./FooterContact.vue";
+import FooterStatement from "./FooterStatement.vue";
 
 import bannerList from "@/assets/bannerList.json";
 import menu from "@/assets/menuCard.json";
